8b22182de8a116ac2997065d139c512847a8e0f3
Henning <hjb.codeforge@gmail.com>
2025-09-18 07:21:52 -0400

    24. A Needle in the Haystack

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -514,7 +514,21 @@
 //     return word.split('').reverse().join('')
 //   }).join(' ')
 // }
-function reverseWords(str) {
-  return str.split('').reverse().join('').split(' ').reverse().join(' ')
+// function reverseWords(str) {
+//   return str.split('').reverse().join('').split(' ').reverse().join(' ')
+// }
+// console.log(reverseWords('double  spaces'))
+
+//24. A Needle in the Haystack
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+// Example(Input --> Output)
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+// Note: In COBOL, it should return "found the needle at position 6"
+
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.findIndex(x => x === "needle")}`
 }
-console.log(reverseWords('double  spaces'))
+console.log(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']))
