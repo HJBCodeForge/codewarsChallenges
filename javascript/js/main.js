8a32a1be1503443231417d4378f57a860fb01fb3
Henning <hjb.codeforge@gmail.com>
2025-09-25 07:43:27 -0400

    25. DNA to RNA Conversion

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -528,7 +528,25 @@
 // ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
 // Note: In COBOL, it should return "found the needle at position 6"
 
-function findNeedle(haystack) {
-  return `found the needle at position ${haystack.findIndex(x => x === "needle")}`
+// function findNeedle(haystack) {
+//   return `found the needle at position ${haystack.findIndex(x => x === "needle")}`
+// }
+// console.log(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']))
+
+//25. DNA to RNA Conversion
+//Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+//Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+// Create a function which translates a given DNA string into RNA.
+// For example:
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+//Medthod1
+function DNAtoRNA(dna) {
+  return dna.split('').map(x => x === 'T' ? x = 'U' : x).join('')
 }
-console.log(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']))
+//Method2
+function DNAtoRNA(dna) {
+  return dna.replaceAll('T','U');
+}
+console.log(DNAtoRNA("GCAT"))
