d24a788d6544fbd99501c20163885d7606f75c40
Henning <hjb.codeforge@gmail.com>
2025-09-29 06:45:49 -0400

    26. In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -542,11 +542,31 @@
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
 //Medthod1
-function DNAtoRNA(dna) {
-  return dna.split('').map(x => x === 'T' ? x = 'U' : x).join('')
+// function DNAtoRNA(dna) {
+//   return dna.split('').map(x => x === 'T' ? x = 'U' : x).join('')
+// }
+// //Method2
+// function DNAtoRNA(dna) {
+//   return dna.replaceAll('T','U');
+// }
+// console.log(DNAtoRNA("GCAT"))
+
+//26. In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Examples
+// highAndLow("1 2 3 4 5"); // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers){
+  let lowestNum = numbers.split(' ').sort(((a,b) => a-b)).shift()
+  let highestNum = numbers.split(' ').sort(((a,b) => a-b)).pop()
+  let highandLow = `${highestNum} ${lowestNum}`
+  return highandLow
 }
-//Method2
-function DNAtoRNA(dna) {
-  return dna.replaceAll('T','U');
-}
-console.log(DNAtoRNA("GCAT"))
+
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
