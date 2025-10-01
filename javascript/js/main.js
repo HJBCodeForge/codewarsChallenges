bf8a800db57b3339d919e820bc69e8f0a201b060
Henning <hjb.codeforge@gmail.com>
2025-10-01 09:10:44 -0400

    28. Find the next perfect square

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -575,8 +575,27 @@
 // Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
 //[Make sure you type the exact thing I wrote or the program may not execute properly]
 
-function greet(name){
-  return `Hello, ${name} how are you doing today?`
+// function greet(name){
+//   return `Hello, ${name} how are you doing today?`
+// }
+
+// console.log(greet("Ryan"))
+
+//28. Find the next perfect square!
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+// Examples ( Input --> Output )
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1  #  because 114 is not a perfect square
+
+function findNextSquare(sq) {
+  let prefectSqr = Number.isInteger(Math.sqrt(sq)) ? (Math.sqrt(sq) + 1) * (Math.sqrt(sq) + 1) : -1
+  return prefectSqr
 }
 
-console.log(greet("Ryan"))
+console.log(findNextSquare(121))
